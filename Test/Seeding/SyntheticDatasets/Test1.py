519e405b22247ed02a2472a7bf1515ead358a05f
DonPedroRockall <p.sabini97@gmail.com>
2021-10-26 17:11:59 +0200

    TotalNodeClosure: ora rimuove il nodo invece dell'edge

--- Test/Seeding/SyntheticDatasets/Test1.py
+++ Test/Seeding/SyntheticDatasets/Test1.py
@@ -1,4 +1,17 @@
 from Test.Common.DatasetGenerator import GenerateRandomGraphTriple
 from Test.Common.DistributionFunctions import *
 from Test.Common.HidingFunctions import *
+from Utilities.DrawGraph import DrawGraph
+
+
+full, part, rec = GenerateRandomGraphTriple(50, 20, 5, distribution_function=UniformDistribution)
+
+print(list(full.edges()))
+print(list(part.edges()))
+print(list(rec.edges()))
+
+
+DrawGraph(full, graph_name="full", physics=False)
+DrawGraph(part, graph_name="part", physics=False)
+DrawGraph(rec, graph_name="rec", physics=False)
 
