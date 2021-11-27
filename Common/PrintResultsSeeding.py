5f79e12e48ba2faf2a08f08c389a5c1a0fa9d9f5
DonPedroRockall <p.sabini97@gmail.com>
2021-11-27 18:01:51 +0100

    - Uniform & Degree distribution graphs created (2000 nodes as standard measure, with 200, 400 and 1000 hidden nodes) - Tests done

--- Common/PrintResultsSeeding.py
+++ Common/PrintResultsSeeding.py
@@ -1,8 +1,13 @@
 from prettytable import PrettyTable
 
 
-def AddRow(t: PrettyTable, k, vr_full, vr_part, vr_recv):
-    t.add_row([k, vr_full, ,vr_part vr_recv])
+def AddRowVoteOnly(t: PrettyTable, k, vr_full, vr_part, vr_recv, vr_real):
+    t.add_row([k, vr_full, vr_part, vr_recv, vr_real])
+    return t
+
+
+def AddRowBoth(t: PrettyTable, k, bg_full, bg_part, bg_recv, bg_real, vr_full, vr_part, vr_recv, vr_real):
+    t.add_row([k, bg_full, bg_part, bg_recv, bg_real, vr_full, vr_part, vr_recv, vr_real])
     return t
 
 
@@ -10,6 +15,11 @@ def Avg(lst: list):
     return sum(lst) / len(lst)
 
 
-def CreateTable():
-    t = PrettyTable(['k', 'VR_Full', 'VR_Part', 'VR_Recv'])
+def CreateTableVoteOnly():
+    t = PrettyTable(['k', 'VR_Full', 'VR_Part', 'VR_Recv', 'VR_Real'])
+    return t
+
+
+def CreateTableBoth():
+    t = PrettyTable(['k', 'BG_Full', 'BG_Part', 'BG_Recv', 'BG_Real', 'VR_Full', 'VR_Part', 'VR_Recv', 'VR_Real'])
     return t
--- Seeding/LT_model.py
+++ Seeding/LT_model.py
@@ -4,14 +4,14 @@ import networkx as nx
 import numpy
 import random
 
-node_thresh = {}
-node_influence = {}
-
 
 # For the LT model, we need node thresholds (assigned randomly in the range [0, 1]), and also need the sum of the
 # in-edge weights for each node to be less than 1
 
 def InitGraphParametersLT(G: nx.DiGraph):
+    node_thresh = {}
+    node_influence = {} # da mettere in RunLT
+
     for node in G.nodes():
         node_thresh[node] = random.uniform(0, 1)
         node_influence[node] = 0
